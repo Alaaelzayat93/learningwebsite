376c78180caef27377fef5816254678c39931adb
alaaelzayat <alaaelzayat93>
2019-10-18 09:28:56 -0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+/*global $,alert,console*/
+
+  //owl-carousel
+$(document).ready(function () {
+    "use strict";
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        rtl: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
+
+
+
+
+//dropdown
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementByclass("navbar").style.top = "0";
+  } else {
+    document.getElementByclass("navbar").style.top = "-50px";
+  }
+}*/
+/*slider*/
+ 
+
+
+
+
+
